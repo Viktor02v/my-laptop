efa8a23b666d6bcc33ab8de8dca07ac362217f74
Viktor Petruk <damnnn261@gmail.com>
2024-04-05 19:41:16 +0300

    add header

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+</script>
+<template>
+	<header class="flex justify-between p-10 border-b border-zinc-500	">
+		<div class="flex items-center">
+			<img src="/logo-laptop-particular.png" alt="logo">
+			<div class="ml-5">
+				<h1 class="font-bold">YOUR LAPTOP</h1>
+				<p class="text-sm text-gray-500">The shop of your gaming dreams</p>
+			</div>
+		</div>
+
+		<div class="flex mx-10">
+			<div class="flex items-center ">
+				<img src="/cart.svg" alt="cart">
+				<p class="ml-2  text-gray-600">1205 UAN</p>
+			</div>
+
+			<div class="flex items-center ml-5 mr-5">
+				<img src="/heart.svg" alt="like">
+				<p class="ml-2  text-gray-600">Tab</p>
+			</div>
+
+			<div class="flex items-center">
+				<img src="/profile.svg" alt="profile">
+				<p class="ml-2  text-gray-600">Profile</p>
+			</div>
+		</div>
+	</header>
+</template>
